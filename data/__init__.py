b1676a4ef1ba783e66b558e8600b076b0af66366
Dreadyyyy <aboutegor8@gmail.com>
2024-12-24 23:41:57 +0300

    Rewrote sql in __init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -4,27 +4,17 @@ import asyncio
 
 async def __start() -> None:
     async with asql.connect("data/users.db") as db:
-        cur = await db.execute(
+        await db.execute(
             """
-            SELECT EXISTS 
+            CREATE TABLE IF NOT EXISTS Users
             (
-            SELECT 1 FROM sqlite_master
-            WHERE name="Users" AND type ="table"
+                username TEXT NOT NULL,
+                chat_id INTEGER NOT NULL,
+                UNIQUE(username, chat_id)
             )
             """
         )
-        if not (await cur.fetchone() or (0,))[0]:
-            await db.execute(
-                """
-                CREATE TABLE Users
-                (
-                    username TEXT,
-                    chat_id INTEGER,
-                    unique(username, chat_id)
-                )
-                """
-            )
-            await db.commit()
+        await db.commit()
 
 
 asyncio.run(__start())
